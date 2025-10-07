e82405d70bf679c7d36637d12150002ea90347e7
Raihan OiOi <rayaindonesia599@gmail.com>
2025-10-08 05:47:36 +0700

    memperbaiki tampilan

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 // Script//
 
+
+
 // Mengambil id
 const PokemonContainer = document.getElementById('pokemon-container');
 
 // Fungsi untuk mengambil data pokemon
 async function getPokemon(id) {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);//Mengambil data dari api
         if (!response.ok){
             throw new Error(`${response.status} ${response.statusText}`);
         }
         const data = await response.json();
-        // console.log(data) 
-        return {
+        // console.log(data) untuk melihat data di console
+
+        return {//Mengemballikan data yang dibutuhkan
             name: data.name,
             image: data.sprites.front_default,
         };
@@ -23,29 +26,30 @@ async function getPokemon(id) {
 
 
 // Fungsi menampilkan data pokemon
-setTimeout(() => {
+setTimeout(() => {//Untuk merender data
+
     async function displayPokemon(){
-        for (let i = 1; i<= 100; i++){
+        for (let i = 1; i<= 50; i++){//Looping 100 card pokemon
             try{
                 const pokemon = await getPokemon(i);
                 const card = document.createElement('div');
                 const link = document.createElement('a');
-                card.className = 'pokemon-card';   
+                card.className = 'pokemon-card';//Menambahkan class pada card
         
                 card.innerHTML = `
                     <img src= "${pokemon.image}" alt="${pokemon.name}">
-                    <a href="#">${pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</a> 
+                    <a href="#">${pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</a>
                 `;    
-                PokemonContainer.appendChild(card); 
+                PokemonContainer.appendChild(card);//Menambahkan card pada container
 
             }catch (error){// Mengecek error
                 const errorMessage = document.createElement('p');
                 errorMessage.innerText = `${error}`;
-                PokemonContainer.appendChild(errorMessage);
+                PokemonContainer.appendChild(errorMessage); //Menampilkan error pada container
             }    
         }    
     }    
-    displayPokemon();
+    displayPokemon();// Memanggil fungsi display pokemon
 },1500)    
 
 
@@ -54,40 +58,39 @@ const searchInput = document.getElementById('Search-input')
 const resultDiv = document.getElementById('result');
 
 searchInput.addEventListener('input', async () => {
-    const query = searchInput.value.trim();
+    const query = searchInput.value.trim();//Mengosongkan spasi di awal dan akhir input
         
     //Mengosongkan nilai input
-    resultDiv.innerHTML = '';   
+    resultDiv.innerHTML = ''.charAt(0)+''.slice(1);
     resultDiv.style.display = 'none';
 
-    if (!query) {
+    if (!query) {//Jika input kosong akan mengosongkan result
         resultDiv.innerHTML = ``;
         resultDiv.style.display = `none`;
         return;
-    }
-
-    try {
-        const responsePokemon = await getPokemon(query);
-        
-        if (responsePokemon) {
-            resultDiv.innerHTML = `
-                <div>
-                    <h2>${responsePokemon.name.charAt(0).toUpperCase() + responsePokemon.name.slice(1)}</h2>
-                    <img src= "${responsePokemon.image}" alt="${responsePokemon.name}">
-                    <a href ='#'>Lihat detail pokemon</a>
-                </div>
-                `;
+    } else {
+        try {
+            const responsePokemon = await getPokemon(query); //Mengambil data pokemon berdasarkan input
             
-        } else{
+            if (responsePokemon) {
+                resultDiv.innerHTML = `
+                    <div>
+                        <h2>${responsePokemon.name.charAt(0).toUpperCase() + responsePokemon.name.slice(1)}</h2>
+                        <img src= "${responsePokemon.image}" alt="${responsePokemon.name}">
+                        <a href ='#'>Lihat detail pokemon</a>
+                    </div>
+                    `;
+                
+            } else{
+                resultDiv.innerHTML = `<p>Pokemon tidak ditemukan</p>`;
+            }
+    
+        } catch (error) {
             resultDiv.innerHTML = `<p>Pokemon tidak ditemukan</p>`;
+            console.log(error);
         }
-
-    } catch (error) {
-        resultDiv.innerHTML = `<p>Pokemon tidak ditemukan</p>`;
-        console.log(error);
+        resultDiv.style.display = 'block'; //Jika ada data pokemon akan menampilkan result
     }
-
-    resultDiv.style.display = 'block';
 });
 
 
